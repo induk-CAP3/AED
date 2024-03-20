9ed1e85412f4315a7323dd46c70dc47c87336f5c
hakhyun98 <xorjsrla123@naver.com>
2024-03-20 19:36:06 +0900

    project setting

---
+++ src/main/java/com/example/aed/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.example.aed.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class TestController {
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
